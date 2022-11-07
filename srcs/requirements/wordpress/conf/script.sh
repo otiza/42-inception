d739da17a210eef58cee71bae4a8c9da0bd2f11f
root <root@DESKTOP-PN5SL3H.localdomain>
2022-11-07 15:59:04 +0100

    php is wld lqhba

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -9,5 +9,6 @@ mv ./wordpress /var/www/html/
 rm -rf /var/www/html/index.nginx-debian.html 
 echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 
-
+service php7.3-fpm start
+service php7.3-fpm stop
 /usr/sbin/php-fpm7.3 -F -R
