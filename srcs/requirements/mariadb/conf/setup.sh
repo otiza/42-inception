92fd19ad94894177f261ca082b39c37cabad02b9
root <root@DESKTOP-PN5SL3H.localdomain>
2022-11-08 14:34:09 +0100

    ll

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-mkdir /var/run/mysqld 
-touch /var/run/mysqld/mysqld.sock
-chown -R mysql /var/run/mysqld
-service mysql start
-mysql -u root -e  "CREATE DATABASE IF NOT EXISTS ${DB_NAME}; flush privileges;"
-mysql -u root -e "grant all privileges on *.* to '${USER}'@'%' IDENTIFIED BY '${PASSWORD}'; flush privileges;"
-mysql < /wp_databases.sql -u ${USER} -p${PASSWORD} ${DB_NAME}
-mysqladmin -u root -pPASSWORD shutdown
+if [ "$(ls -A /var/lib/mysql/${DBNAME})" ]; then
+    echo "Database already set up"
+else    
+    mkdir /var/run/mysqld 
+    touch /var/run/mysqld/mysqld.sock
+    chown -R mysql /var/run/mysqld
+    service mysql start
+    mysql -u root -e  "CREATE DATABASE IF NOT EXISTS ${DB_NAME}; flush privileges;"
+    mysql -u root -e "grant all privileges on *.* to '${USER}'@'%' IDENTIFIED BY '${PASSWORD}'; flush privileges;"
+    mysql < /wp_databases.sql -u ${USER} -p${PASSWORD} ${DB_NAME}
+    mysqladmin -u root -pPASSWORD shutdown
 #service mysql stop
-
+fi
 mysqld_safe
