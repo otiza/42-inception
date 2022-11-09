35fefb83d9a44515c3c3c81e806b5e1bf07f67ee
Abdessamad Maaiza <amaaiza@e2r4p3.1337.ma>
2022-11-09 23:27:31 +0100

    11

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -9,7 +9,8 @@ else
     service mysql start
     mysql -u root -e  "CREATE DATABASE IF NOT EXISTS ${DB_NAME}; flush privileges;"
     mysql -u root -e "grant all privileges on *.* to '${USER}'@'%' IDENTIFIED BY '${PASSWORD}'; flush privileges;"
-    muysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO `root`@`localhost` IDENTIFIED BY '${DBROOT_PASS}' WITH GRANT OPTION;
+    mysql -u root -e "USE mysql; UPDATE user SET plugin='mysql_native_password' WHERE User='root';FLUSH PRIVILEGES;"
+    muysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO `root`@`localhost` IDENTIFIED BY '${DBROOT_PASS}';
 FLUSH PRIVILEGES;"
     mysql < /wp_databases.sql -u ${USER} -p${PASSWORD} ${DB_NAME}
     mysqladmin -u root -pPASSWORD shutdown
